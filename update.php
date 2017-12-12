dfb85a593b14cb36eed26933496f031f8ddf2d49
burningknight7 <burningknight7@gmail.com>
2017-12-12 11:45:25 +0530

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>
-		Update
+		Update PHP
 	</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
